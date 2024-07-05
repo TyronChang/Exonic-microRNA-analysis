0643ff6cfd002c7b5069b3a7cc626f240450078e
TyronChang <tyronchang2@gmail.com>
2024-07-05 00:33:40 -0500

    change human_csv.sh file

--- Human_bash_script/human_csv.sh
+++ Human_bash_script/human_csv.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#### This script will move the UCSC data into the folder called UCSC_data_Human
+#### This script will move all csv data into the folder called Human_csv_file
 #### Run this shell script after all the analysis is done.
-mkdir UCSC_data_Human
-mkdir Human_csv_file
 
-mv human_genes_010324_NCBI.csv UCSC_data_Human
 mv *.csv Human_csv_file 
